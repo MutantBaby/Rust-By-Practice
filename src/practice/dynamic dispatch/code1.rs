11575debe51eb2217220d76aff5fdf4fda216e30
MutantBaby <shaheryarbakir998877@gmail.com>
2023-12-04 20:31:41 +0500

    Setting up debugging

--- src/practice/dynamic dispatch/code1.rs
+++ src/practice/dynamic dispatch/code1.rs
@@ -9,21 +9,6 @@ trait SpellChecker {
     fn check(&self, input: &str) -> Vec<Change>;
 }
 
-fn spell_check<C>(input: &str, spell_checker: C) -> String
-where
-    C: SpellChecker,
-{
-    let mut result: String = input.to_owned();
-
-    for change in spell_checker.check(input) {
-        apply_change(&mut result, change);
-    }
-
-    result
-}
-
-fn apply_change(result: &mut String, change: Change) {}
-
 struct NoopSpellChecker;
 
 impl SpellChecker for NoopSpellChecker {
@@ -43,17 +28,48 @@ impl SpellChecker for AntiSpaceChecker {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn static_spell_check<C>(input: &str, spell_checker: C) -> String
+where
+    C: SpellChecker,
+{
+    let mut result: String = input.to_owned();
 
-    #[test]
-    fn it_works() {
-        let test: &str = "Hello, IM here";
-        let result: String = spell_check(test, NoopSpellChecker);
-        assert!(result == test);
-
-        // let result: String = spell_check(test, AntiSpaceChecker);
-        // assert!(result == test);
+    for change in spell_checker.check(input) {
+        apply_change(&mut result, change);
     }
+
+    result
+}
+
+fn dynamic_spell_check(input: &str, spell_checker: &dyn SpellChecker) -> String {
+    let mut result: String = input.to_owned();
+
+    for change in spell_checker.check(input) {
+        apply_change(&mut result, change);
+    }
+
+    result
+}
+
+fn apply_change(result: &mut String, change: Change) {}
+
+fn it_works() {
+    let test: &str = "Hello, IM here";
+
+    let result: String = static_spell_check(test, NoopSpellChecker);
+    assert!(result == test);
+
+    let result: String = dynamic_spell_check(test, &NoopSpellChecker);
+    assert!(result == test);
+
+    let spell_checkers: Vec<Box<dyn SpellChecker>> =
+        vec![Box::new(NoopSpellChecker), Box::new(AntiSpaceChecker)];
+
+    for checker in spell_checkers {
+        dynamic_spell_check(test, checker.as_ref());
+    }
+}
+
+fn main() {
+    it_works();
 }
